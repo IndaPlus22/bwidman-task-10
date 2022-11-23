3b19fa90c4b5ccdf14c50bdce935084cae3289f6
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2022-11-23 01:43:55 +0100

    Weak attempt

--- go.pl
+++ go.pl
@@ -1,3 +1,6 @@
+% Load example
+load_board('alive_example.txt', B).
+
 % Taken and modified from https://gist.github.com/MuffinTheMan/7806903
 % Get an element from a 2-dimensional list at (Row,Column)
 % using 1-based indexing.
@@ -6,16 +9,21 @@ nth1_2d(Row, Column, List, Element) :-
     nth1(Column, SubList, Element).
 
 % Reads a file and retrieves the Board from it.
-load_board(BoardFileName, Board):-
+load_board(BoardFileName, Board) :-
     see(BoardFileName),     % Loads the input-file
     read(Board),            % Reads the first Prolog-term from the file
     seen.                   % Closes the io-stream
 
 % Checks whether the group of stones connected to
 % the stone located at (Row, Column) is alive or dead.
-check_alive(Row, Column, Board):-
+is_alive(Row, Column, Board) :-
     nth1_2d(Row, Column, Board, Stone),
-    (Stone = b; Stone = w). % Example statement so that the function returns. Substitute this with your own logic
+    group(Row, Column, Board, Stone).
 
-% Load example
-load_board('alive_example.txt', B).
+group(_, _, _, e).
+group(Row, Column, Board, Stone):-
+    nth1_2d(Row - 1, Column, Board, Stone);
+    nth1_2d(Row + 1, Column, Board, Stone);
+    nth1_2d(Row, Column - 1, Board, Stone);
+    nth1_2d(Row, Column + 1, Board, Stone);
+    group(Row, Column, Board, e).
