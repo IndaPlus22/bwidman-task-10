5bb7b0266fc87f7dc8a1b769c5304ce379d0f4c1
Benjaneb <benjaneb1@gmail.com>
2022-11-25 11:19:21 +0100

    Keeps recursing to infinity x_x

--- go.pl
+++ go.pl
@@ -1,6 +1,3 @@
-% Load example
-load_board('alive_example.txt', B).
-
 % Taken and modified from https://gist.github.com/MuffinTheMan/7806903
 % Get an element from a 2-dimensional list at (Row,Column)
 % using 1-based indexing.
@@ -17,13 +14,32 @@ load_board(BoardFileName, Board) :-
 % Checks whether the group of stones connected to
 % the stone located at (Row, Column) is alive or dead.
 is_alive(Row, Column, Board) :-
-    nth1_2d(Row, Column, Board, Stone),
-    group(Row, Column, Board, Stone).
+    nth1_2d(Row, Column, Board, Stone),     % Get stone at x,y
+    liberty(Row, Column, Board, Stone).
 
-group(_, _, _, e).
-group(Row, Column, Board, Stone):-
-    nth1_2d(Row - 1, Column, Board, Stone);
-    nth1_2d(Row + 1, Column, Board, Stone);
-    nth1_2d(Row, Column - 1, Board, Stone);
-    nth1_2d(Row, Column + 1, Board, Stone);
-    group(Row, Column, Board, e).
+% Checks if any surrounding stone is empty
+liberty(Row, Column, Board, e) :-
+    Up is Row - 1,
+    Down is Row + 1,
+    Left is Column - 1,
+    Right is Column + 1,
+    (nth1_2d(Up, Column, Board, e);
+    nth1_2d(Down, Column, Board, e);
+    nth1_2d(Row, Left, Board, e);
+    nth1_2d(Row, Right, Board, e)),
+    !.
+liberty(Row, Column, Board, Stone) :-
+    liberty(Row, Column, Board, e);         % Check for empty slot around stone
+    (nth1_2d(Row, Column, Board, Stone),    % Check if new slot if same color (redundant on first iteration)
+    Up is Row - 1,
+    Down is Row + 1,
+    Left is Column - 1,
+    Right is Column + 1,
+    liberty(Up, Column, Board, Stone);
+    liberty(Down, Column, Board, Stone);
+    liberty(Row, Left, Board, Stone);
+    liberty(Row, Right, Board, Stone)).
+
+main(Row, Column) :-
+    load_board('alive_example.txt', Board),     % Load example
+    is_alive(Row, Column, Board).
